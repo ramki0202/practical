52c01273600a5c2ae4285d4106d13bd4c74ef696
Ramki <ram.ramki0202@gmail.com>
2020-05-27 09:21:19 +0530

    Average number

--- average numbers.c
+++ average numbers.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+ float a,b,c,d,e,f,g,h;
+ printf("enter the Value of n:");
+ scanf("%f %f %f %f %f %f",&a,&b,&c,&d,&e,&f);
+ g=(a+b+c+d+e+f)/6;
+ h=(a+b+c+d+e+f);
+ printf("\nAverage Value:%.2f",g);
+ printf("\nTotal Value:%.1f",h);
+ return 0;
+    }
