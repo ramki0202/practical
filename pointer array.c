44a25f2fe596e4f194625e8498b76024ba2677bd
Ramki <ram.ramki0202@gmail.com>
2020-08-16 21:43:03 +0530

    pinter variable array

---
+++ pointer array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int *b,i,sum=0;
+    b=&a;
+    for(i=0;i<5;i++){
+    printf("\n%u %d",*(b+i), b+i);
+    }
+    for(i=0;i<5;i++)
+    {
+        sum+=b[i];
+    }
+
+    printf("\n%d",sum);
+
+    for(i=0;i<5;i++)
+    {
+        sum=1;
+        sum+=b[i];
+
+    }
+     printf("\n%u %d",*(b+i), b+i);
+    for(i=0;i<5;i++){
+    printf("\n%u %d",*(b+i), b+i);
+    }
+
+
+}
