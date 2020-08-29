6d9b2d0ffc00066f9743cabacfeaab0ae3972ed4
Ramki <ram.ramki0202@gmail.com>
2020-08-29 07:48:14 +0530

    Practicals

--- fibanaccci number.c
+++ fibanaccci number.c
@@ -2,9 +2,12 @@
 int main()
 {
    int n,t1=0,t2=1,sum=0,i=1;
+
    printf("Enter your increment number:");
    scanf("%d",&n);
+
    printf("Fibonacci Number is:");
+
    for(i=1;i<=n;i++)
    {
        printf(" %d", t1);
